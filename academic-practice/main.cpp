94e0a9ab4d9b8a071e75084bb3fa8fb57a4be62e
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-11-12 21:43:09 +0300

    feat: task2-3 done

--- academic-practice/main.cpp
+++ academic-practice/main.cpp
@@ -12,15 +12,15 @@
 
 int main()
 {
-    alias::laptop_map lmap = create_laptop_map(13);
-
-    alias::laptop_multimap lmmap(lmap.begin(), lmap.end());
-    lmmap.emplace(laptop_key("Factory 1", "Model 1"), laptop());
-    lmmap.emplace(laptop_key("Factory 1", "Model 1"), laptop());
-    lmmap.emplace(laptop_key("Factory 2", "Model 2"), laptop());
-    lmmap.emplace(laptop_key("Factory 2", "Model 2"), laptop());
-
-    msl::print(lmmap);
+    // alias::laptop_map lmap = create_laptop_map(13);
+    //
+    // alias::laptop_multimap lmmap(lmap.begin(), lmap.end());
+    // lmmap.emplace(laptop_key("Factory 1", "Model 1"), laptop());
+    // lmmap.emplace(laptop_key("Factory 1", "Model 1"), laptop());
+    // lmmap.emplace(laptop_key("Factory 2", "Model 2"), laptop());
+    // lmmap.emplace(laptop_key("Factory 2", "Model 2"), laptop());
+    //
+    // msl::print(lmmap);
 
 
 #if 0
@@ -38,4 +38,24 @@ int main()
     task2_2_4_presentation(lmmap);
     task2_2_7_presentation(lmmap, {"Factory 1", "Model 1"});
 #endif
+
+#if 0
+
+#endif
+
+
+    // msl::print(create_laptop_vector(13));
+    auto pqr = create_laptop_prq(13);
+    msl::print(pqr);
+    // auto laptops = create_laptop_vector(13);
+    // std::priority_queue<laptop> prq;
+    // for (auto lap : laptops) {
+    //     prq.push(lap);
+    // }
+
+
+
+
+
+
 }
