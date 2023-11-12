c2a5cc013e1bad152e2bd7be87c3e846f16aedc8
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-11-12 06:32:00 +0300

    wip: [class laptop] remove fields factory and model

--- academic-practice/laptop.hpp
+++ academic-practice/laptop.hpp
@@ -13,9 +13,7 @@
 class laptop {
 public:
 #if 1
-    laptop (std::string factory_ = "unknown", std::string model_ = "unknown") :
-        factory(factory_),
-        model(model_),
+    laptop () :
         price(msl::rand<double>(1.0, 1000.99)[0]),
         diagonal(msl::rand<double>(1.0, 20.11)[0]),
         coresNum(msl::rand<int>(1, 100)[0]),
@@ -25,9 +23,7 @@ public:
 #endif
 
 #if 0
-    laptop (std::string factory_ = "unknown", std::string model_ = "unknown") :
-        factory(factory_),
-        model(model_),
+    laptop () :
         price(msl::random<double>(1.0, 1000.99)),
         diagonal(msl::random<double>(1.0, 20.11)),
         coresNum(msl::random<int>(1,100)),
@@ -37,22 +33,17 @@ public:
 #endif
 
 private:
-    static int id_generator() {
+    static int id_generator() { // mb I should remove static here, it's about design
         static int id = 0;
         return ++id;
     }
 
 public:
-    const   std::string factory;
-    const   std::string model;
             double      price;
     const   double      diagonal;
     mutable int         coresNum;
-            int         ram;
+    mutable int         ram;
     const   int         id;
-
-private:
-    static int laptop_id;
 };
 
 
@@ -63,10 +54,10 @@ namespace msl {
         using std::left;
 
         printf("\n==============================\n");
-        cout.width(10); cout << left << "Factory: "; cout << lap.factory << "\n";
-        cout.width(10); cout << left << "Model: "; cout << lap.model;
+        // cout.width(10); cout << left << "Factory: "; cout << lap.factory << "\n";
+        // cout.width(10); cout << left << "Model: "; cout << lap.model;
 
-        cout.width(30-lap.model.size()+1-10); cout << right << std::format("(id: {})\n", lap.id);
+        cout.width(30); cout << right << std::format("(id: {})", lap.id) << "\n";
 
         cout.width(10); cout << left << "Price: ";    cout << lap.price    << "\n";
         cout.width(10); cout << left << "Diagonal: "; cout << lap.diagonal << "\n";
