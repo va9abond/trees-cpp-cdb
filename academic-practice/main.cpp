61ba0934677cb7e2817efd20274996eb3dd1a926
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2023-11-21 07:21:05 +0300

    wip: add task2_6presentation

--- academic-practice/main.cpp
+++ academic-practice/main.cpp
@@ -3,74 +3,65 @@
 #include <vector>
 
 #include "view.hpp"
-#include "avltree.hpp"
 
 
-int recursin (int v) {
-    static int val = 555;
-    if (v == 0) {
-        return val;
-    } else {
-        printf("v: %d\n", v);
-        if (val == 555) {
-            printf("DKLFJDKFJDKLJFDLKJF\n");
-        }
-        val = v + 1;
-        return recursin(v-1);
-    }
-}
+#define TASK_2_1 1
+#define TASK_2_2 1
+#define TASK_2_3 1
+#define TASK_2_4 1
+#define TASK_2_5 1
+#define TASK_2_6 1
+
 
 int main() {
-alias::laptop_map lmap = create_laptop_map(13);
-msl::print(lmap);
+
+    alias::laptop_map lmap = create_laptop_map(13);
 
 
-alias::laptop_multimap lmmap(lmap.begin(), lmap.end());
-lmmap.emplace(laptop_key("Factory 1", "Model 1"), laptop());
-lmmap.emplace(laptop_key("Factory 1", "Model 1"), laptop());
-lmmap.emplace(laptop_key("Factory 2", "Model 2"), laptop());
-lmmap.emplace(laptop_key("Factory 2", "Model 2"), laptop());
+    alias::laptop_multimap lmmap(lmap.begin(), lmap.end());
+    lmmap.emplace(laptop_key("Factory 1", "Model 1"), laptop());
+    lmmap.emplace(laptop_key("Factory 1", "Model 1"), laptop());
+    lmmap.emplace(laptop_key("Factory 2", "Model 2"), laptop());
+    lmmap.emplace(laptop_key("Factory 2", "Model 2"), laptop());
 
 
-msl::print(lmmap);
+#if TASK_2_1
+        msl::print(lmap);
 
-#if 1
-    task2_1_1_presentation(13);
-    task2_1_2_presentation(lmap);
-    msl::print(lmap);
-    task2_1_4_presentation(lmap);
-    task2_1_5_presentation(lmap);
-    task2_1_6_presentation(lmap);
+        task2_1_1_presentation(13);
+        task2_1_2_presentation(lmap);
+        msl::print(lmap);
+        task2_1_4_presentation(lmap);
+        task2_1_5_presentation(lmap);
+        task2_1_6_presentation(lmap);
 #endif
 
-#if 1
-    task2_2_1_presentation();
-    task2_2_2_presentation(lmmap);
-    task2_2_4_presentation(lmmap);
-    task2_2_7_presentation(lmmap, {"Factory 1", "Model 1"});
+#if TASK_2_2
+        msl::print(lmmap);
+
+        task2_2_1_presentation();
+        task2_2_2_presentation(lmmap);
+        task2_2_4_presentation(lmmap);
+        task2_2_7_presentation(lmmap, {"Factory 1", "Model 1"});
 #endif
 
-#if 1
-    task2_3_presentation(13);
+#if TASK_2_3
+        task2_3_presentation(13);
 #endif
 
-#if 1
-    task2_4presentation();
-    task2_5presentation();
+#if TASK_2_4
+        task2_4presentation();
 #endif
 
-    // Tree_node<laptop> node1;
-    // msl::print(node1.Myval);
+#if TASK_2_5
+        task2_5presentation();
+#endif
 
-    // auto node2 = Tree_node<laptop_key>::Buy_node(nullptr, "ddfa", "dfjw");
-    // auto node3 = Tree_node<laptop_key>::Buy_node(nullptr, "213", "cnwooj;");
-    //
-    // msl::print(node2->Myval);
-    // msl::print(node3->Myval);
-    //
-    // Tree_node<laptop_key>::Free_node(node2);
-    // Tree_node<laptop_key>::Free_node(node3);
-    // std::set<laptop_key> set;
-    // set.emplace("djlfsjd", "fjladkfj");
-    // msl::print(*set.begin());
+#if TASK_2_6
+        // [NOTE]: Store not laptops, but ints to easier
+        //         representation, all we need from stored
+        //         type 1. it should be object type
+        //              2. operator< or other binary compare Predicate
+        task2_6_presentation();
+#endif
 }
